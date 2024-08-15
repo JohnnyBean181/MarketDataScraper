17e93b0af6604cfe24974a0a4c4c13b59199b211
johnnybean <492569493@qq.com>
2024-08-15 23:01:30 +0800

    [dev_bse]implement eom_bse.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import eom_sse, eom_szse
+import eom_sse, eom_szse, eom_bse
 
 
 def main():
     # eom_sse.execute()
-    eom_szse.execute()
+    # eom_szse.execute()
+    eom_bse.execute()
 
 if __name__ == "__main__":
     main()
