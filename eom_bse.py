91d139cb6cf8dfafb49e89f84297ece4954d5a8c
johnnybean <492569493@qq.com>
2024-08-16 17:43:28 +0800

    [dev_bse]add PE value

--- eom_bse.py
+++ eom_bse.py
@@ -40,7 +40,7 @@ def transform(data_rows, data_type: str) -> dict:
     return data_dict
 
 
-def extract(bse_webpage):
+def extract(bse_webpage, bse_webpage2):
     """
     This function aims to extract the required
     information from the website and save it to a data frame. The
@@ -66,7 +66,29 @@ def extract(bse_webpage):
     # 读取表格中的数据，每一个div中包含一个数据
     data_rows = data_list.find_elements(By.CLASS_NAME, "market_info_detail")
     # 数据格式规范化
-    data_dict = transform(data_rows, "北交所")
+    data_dict = transform(data_rows, "北证")
+
+    log_progress("Step 4/4. Loading webpage 2...")
+    driver.get(bse_webpage2)  # 加载页面
+    driver.maximize_window()
+
+    log_progress("Step 5/6. Switching to tab of MarketStar...")
+    # 选取表格面板中的“月报”按钮，点击后，等待3秒以加载“月报”数据
+    nav_panel = driver.find_element(by=By.ID, value="ulTab")
+    tabs = nav_panel.find_elements(by=By.TAG_NAME, value="li")
+    tabs[2].click()
+    time.sleep(3)
+
+    log_progress("Step 5/6. Retrieving data of MarketStar...")
+    # 读取“市盈率”数据
+    table = driver.find_element(By.ID, 'neeqStatistical')
+    trs = table.find_elements(By.TAG_NAME, 'tr')
+    tr = trs[-2]
+    tds = tr.find_elements(By.TAG_NAME, 'td')
+    td_text = tds[3].text
+
+    log_progress("Step 5/6. Retrieving data of MarketStar...")
+    data_dict["AVG_PE"] = float(td_text.strip())
     df = pd.DataFrame(data_dict, index=[0])
 
     log_progress("Step 4/4. Data extraction complete...")
@@ -91,6 +113,7 @@ def execute():
     config.read('config.ini')
     # 提取常规数据，如网页等
     url_bse = config['DEFAULT']['url_bse']
+    url_bse2 = config['DEFAULT']['url_bse2']
     # 提取数据库连接信息
     user = config['database']['user']
     password = config['database']['password']
@@ -100,7 +123,7 @@ def execute():
     table_name = config['database']['table_name']
 
     """  从交易所首页抓取数据  """
-    df_transformed = extract(url_bse)
+    df_transformed = extract(url_bse, url_bse2)
     print(df_transformed)
 
     """  将抓取的数据存入数据库  """
@@ -112,6 +135,6 @@ def execute():
     load_to_MySQL_on_Cloud(df_transformed, engine, table_name)
 
     """  从数据库读取数据并打印在控制台  """
-    Q3 = f"SELECT Market_Type from {table_name}"
-    df_retrieved = run_query(Q3, engine)
-    print(df_retrieved)
+    # Q3 = f"SELECT Market_Type from {table_name}"
+    # df_retrieved = run_query(Q3, engine)
+    # print(df_retrieved)
